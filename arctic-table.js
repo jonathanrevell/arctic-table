38ecbafb9a383b698f43364816d7e85a51910366
Jonathan Revell H <jonathan.revell.hopkins@gmail.com>
2015-07-17 16:34:15 -0600

    Made root an option that can be provided

--- arctic-table.js
+++ arctic-table.js
@@ -1,26 +1,38 @@
 (function(exports, TableSplicer){
 
-  var ArcticTable = function( table ) {
+  var ArcticTable = function( table, root ) {
     this.$table   = $(table);
     this.$wrapper = null;
-    this.$root    = null;
+    this.$root    = root || null;
 
-    this.setupDOM();
+    this.frozenColumns = [];
+
+    this.setupDOM( root );
+    this.setupResizeListener();
   };
 
   ArcticTable.prototype = {
-    setupDOM: function() {
+    setupDOM: function( root ) {
       var rootTemplate = "<div class='arctic-table'></div>",
           wrapTemplate = "<div class='arctic-table-wrapper'></div>";
 
-      this.$table.wrap(rootTemplate);
-      this.$root = this.$table.parent();
+      if(!root) {
+        this.$table.wrap(rootTemplate);
+        this.$root = this.$table.parent();
+      } else {
+        this.$root.addClass("arctic-table");
+      }
 
       this.$table.wrap(wrapTemplate);
     },
     freezeColumn: function( col ) {
       var $frozenColumn = TableSplicer.cloneColumn( this.$table, col );
       $frozenColumn.addClass('frozen-column');
+
+      this.frozenColumns.push({
+        index: col,
+        el: $frozenColumn
+      });
 
       this.toggleColumnVisibility( col, true);
       this.$root.append($frozenColumn);
@@ -30,6 +42,32 @@
           state = makeVisible ? 'visible' : 'hidden';
 
       this.$table.find('td' + partialSelector + ', th' + partialSelector).css('visibility', state);
+    },
+    rebuildFrozenColumns: function() {
+      var self = this,
+          indexes;
+
+      indexes = this.removeFrozenColumnsFromDOM();
+      _.each( indexes, function( col ) {
+        self.freezeColumn(col);
+      });
+    },
+    removeFrozenColumnsFromDOM: function() {
+      var indexes = [];
+      _.each( this.frozenColumns, function( col ) {
+        col.el.remove();
+        col.el = null;
+        indexes.push(col.index);
+      });
+
+      this.frozenColumns = [];
+      return indexes;
+    },
+    setupResizeListener: function() {
+      var self = this;
+      $(document).on('resize', function() {
+        self.rebuildFrozenColumns();
+      });
     }
   };
 
